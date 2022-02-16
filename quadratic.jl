086ba13a5188884318de90c8fb8924efe1c98d05
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-02-16 21:50:27 +0530

    solve for the roots of a quadratic equation

--- quadratic.jl
+++ quadratic.jl
@@ -2,52 +2,43 @@
 # File: quadratic.jl
 # Name: D.Saravanan
 # Date: 15/08/2021
-# Program to solve for the roots of a quadratic equation of the form a*x^2 + b*x + c = 0
 
-function roots(a::Int64, b::Int64, c::Int64)
+""" Program to solve for the roots of a quadratic equation """
 
-    #a :: Float64                # coefficient of x^2 term of equation
-    #b :: Float64                # coefficient of x term of equation
-    #c :: Float64                # constant term of equation
-    #discriminant :: Float64     # discriminant of the equation
-    #imag_part :: Float64        # imaginary part of equation (for complex roots)
-    #real_part :: Float64        # real part of equation (for complex roots)
-    #x1 :: Float64               # first solution of equation (for real roots)
-    #x2 :: Float64               # first solution of equation (for real roots)
+using Printf
+
+function roots(a::Float64, b::Float64, c::Float64)
 
     # calculate discriminant
-    discriminant::Float64 = b^2 - 4*a*c
+    discriminant = b^2 - 4*a*c
 
     # solve for the roots, depending upon the value of the discriminant
     if (discriminant > 0)
         x1 = (-b + sqrt(discriminant))/(2*a)
         x2 = (-b - sqrt(discriminant))/(2*a)
-        println("This equation has two real roots: x1 = ", x1, " and x2 = ", x2)
+        @printf("This equation has two real roots: x1 = %.2f and x2 = %.2f\n", x1, x2)
 
     elseif (discriminant == 0)
         x1 = (-b)/(2*a)
-        println("This equation has two identical real roots: x1 = x2 = ", x1)
+        @printf("This equation has two identical real roots: x1 = x2 = %.2f\n", x1)
 
     else
         real_part = (-b)/(2*a)
         imag_part = sqrt(abs(discriminant))/(2*a)
-        println("This equation has complex roots: x1 = ", real_part, " +i ", imag_part,
-                " and x2 = ", real_part, " -i ", imag_part)
+        @printf("This equation has complex roots: x1 = %.2f + i%.2f and x2 = %.2f - i\
+        %.2f\n", real_part, imag_part, real_part, imag_part)
 
     end
 
 end
 
 # prompt the user for the coefficients of the equation
-println("""This program solves for the roots of a quadratic
-        equation of the form (a*x^2 + b*x + c = 0)""")
+println("This program solves for the roots of a quadratic equation")
 
-#a, b, c = read("Enter the coefficients a, b, and c: ")
-#println(a, b, c)
-
-a, b, c = 2, 3, 4
+print("Enter the coefficients a, b, and c: ")
+a, b, c = [parse(Float64, x) for x in split(readline())]
 
 # print the coefficients
-println("The coefficients a, b, and c are: ", a, b, c)
+@printf("The coefficients a, b, and c are: %.1f, %.1f, %.1f\n", a, b, c)
 
 roots(a, b, c)
